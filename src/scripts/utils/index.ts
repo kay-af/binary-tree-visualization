6be8bb1a5de7f347e61f4c3982e968aa9befa96b
Afridi <kayalafridi@gmail.com>
2024-12-11 01:41:47 +0530

    Redirect from github pages

--- src/scripts/utils/index.ts
+++
@@ -1,4 +0,0 @@
-export { TreeNode } from "./tree-node";
-export { TreeParser, TreeParserError } from "./tree-parser";
-export type { Size } from "./size";
-export { enableMousePan } from "./enable-mouse-pan";
--- src/scripts/utils/size.ts
+++
@@ -1,7 +0,0 @@
-/** Represents size of an element. */
-export interface Size {
-  /** Width of the element. */
-  width: number;
-  /** Height of the element. */
-  height: number;
-}
